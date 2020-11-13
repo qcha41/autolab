6def2fa0e30ca6aca7e1ec6eeddcf55992aea7f5
Quentin Chateiller <q.chateiller@gmail.com>
2020-11-13 11:22:10 +0100

    Removed device.py in autolab\autolab

--- autolab/core/devices.py
+++ autolab/core/devices.py
@@ -10,6 +10,40 @@ def DEMO_local_devices() :
     local_devices['my_remote_PC'] = {'driver':'autolab_SERVER','connection':'VISA','address':'192.192.192.192'}
     return local_devices
 
+
+#from .core.devices import StructureManager, ElementWrapper
+
+
+class DeviceManager():
+    ''' This is the main class of the Devices part.
+    It represents the top level device of the device structure,
+    and as this, it inherits from the ElementWrapper class.
+    The only difference is that its here that the StructureManager class
+    of the session is instantiated, and that this "top level Device" is
+    connected automatically '''
+
+    def __init__(self):
+        # For the top level device, instantiate the StructureManager class
+        self._sm = StructureManager()
+
+    def summary(self):
+        self._sm.print_summary(None)
+
+    # Routines to access sub elements
+    def get(self, attr):
+        return ElementWrapper(self._sm, attr)
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __getitem__(self, attr):
+        return self.get(attr)
+
+    # Routines for autocompletion
+    def __dir__(self):
+        pass
+
+
 class StructureManager() :
 
     ''' This class is the internal interface used to interact with instruments.
