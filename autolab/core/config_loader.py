a58605c1589cbd8a5ef354f76d5176234c6eccc2
bruno066 <bruno.garbin.etu@gmail.com>
2020-12-09 19:26:48 +0100

    add csv files to config_loader.py

--- autolab/core/config_loader.py
+++ autolab/core/config_loader.py
@@ -49,8 +49,104 @@ def build_parser_from_dict(config_dict):
     
     return config_ini
 
+
+def write_dict_to_csv(input_dict,filename):
     
-def convert_ini_to_dataframe(config_ini_path):
+    ''' Take a dict as argument and write a it to a csv file '''
+    
+    import csv
+    
+    assert isinstance(input_dict,dict)
+    
+    # Create another dict to add a first column with device names (key is '')
+    dict_to_save = {}
+    for i,k in input_dict.items():
+        dict_to_save[i] = {}
+        dict_to_save[i].update({'':i})
+        dict_to_save[i].update(input_dict[i])
+    
+    # List all the nested keys (e.g. address, connection) => necessary to csv DictWriter
+    list_inner_keys = []                  
+    for key in dict_to_save.keys():               
+        for key_in in dict_to_save[key].keys():                   
+            if key_in not in list_inner_keys:                    
+                list_inner_keys.append(key_in)
+    
+    # Write the 
+    with open(filename, 'w') as csvfile:
+        fieldnames = list_inner_keys
+        
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for key in dict_to_save.keys():
+            writer.writerow(dict_to_save[key])
+
+
+def read_dict_from_csv(filename):
+    
+    ''' Take the filename to read csv from and returns a 2D dict '''
+    
+    import csv
+    
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        output_dict = {}
+        for row in reader:
+            for key,value in row.items():
+                if key == '':
+                    device = value.strip(' ')
+                    output_dict[device] = {}
+                else:
+                    if value!= '': output_dict[device][key] = value
+    
+    return output_dict
+
+    
+def convert_csv_to_ini(config_csv_path):
+    
+    ''' Take a path to the ini file as input argument and write at same location a xlsx file using pandas '''
+    
+    import csv
+    import configparser
+    
+    assert os.path.exists(config_csv_path), f"File {os.path.basename(config_csv_path)} not found at path {os.path.dirname(config_csv_path)}"
+    assert '.csv' in os.path.basename(config_csv_path), f'Formating error with file {os.path.dirname(config_csv_path)}, ".csv" must be the extension'
+    
+    # Load config csv into 2D dict
+    config_dict = read_dict_from_csv(config_csv_path)
+    
+    # Convert 2D dict to configparser
+    config_ini  = build_parser_from_dict(config_dict)
+    
+    # Save configparser
+    new_filename = config_csv_path.strip('.csv')+'.ini'
+    with open(new_filename, 'w') as f:
+        config_ini.write(f)
+
+
+def convert_ini_to_csv(config_ini_path):
+    
+    ''' Take a path to the ini file as input argument and write at same location a xlsx file using pandas '''
+    
+    import csv
+    import configparser
+    
+    assert os.path.exists(config_ini_path), f"File {os.path.basename(config_ini_path)} not found at path {os.path.dirname(config_ini_path)}"
+    assert '.ini' in os.path.basename(config_ini_path), f'Formating error with file {os.path.dirname(config_ini_path)}, ".ini" must be the extension'
+    
+    # Load config parser
+    config_ini = configparser.ConfigParser()
+    config_ini.read(config_ini_path)
+
+    # Place config parser into a 2D dict
+    config_dict = build_dict_from_parser(config_ini)
+    
+    # Save dict to csv file
+    new_filename = config_ini_path.strip('.ini')+'.csv'
+    write_dict_to_csv(config_dict,new_filename)
+
+    
+def convert_ini_to_xlsx(config_ini_path):
     
     ''' Take a path to the ini file as input argument and write at same location a xlsx file using pandas '''
     
@@ -65,7 +161,7 @@ def convert_ini_to_dataframe(config_ini_path):
     config_ini.read(config_ini_path)
 
     # Place config parser into a 2D dict
-    config_dict = build_dict_from_ini(config_ini)
+    config_dict = build_dict_from_parser(config_ini)
     
     # Convert 2D dict to pd.Dataframe
     config_data_frame = pd.DataFrame(config_dict).T
@@ -75,7 +171,7 @@ def convert_ini_to_dataframe(config_ini_path):
     config_data_frame.to_excel(new_filename)
 
 
-def convert_dataframe_to_ini(config_dataframe_path):
+def convert_xlsx_to_ini(config_dataframe_path):
     
     ''' Take a path to the xlsx file as input argument and write at same location a ini file using configparser '''
     
